noccu/umamusume-db-translate

commit f5b89d40193294345dc2cbad708201c53bd9a5d0

Change strength display in skill data
Add basic ability to edit it as well.
I don't like it but it's a worry for later.

scripts/extract-skill-data.js
@@ -60,8 +60,21 @@ function translateData(sqlData) {
 function translateEffect(type, strength) {
     let effect = DATA_TL.ability_type[type];
     strength = strength / 10000;
-    if (strength > 0) strength = "+" + strength;
-    // if (strength < 40) strength = strength.toLocaleString(undefined, {style: "percent"});
+
+    //todo: find something better, if needed...
+    if (Array.isArray(effect)) {
+        let edit = effect[1];
+        effect = effect[0];
+
+        edit.split(" ").forEach(f => {
+            if (f == "1-") strength = 1 - strength
+            else if (f == "-") strength *= -1
+        })
+    }
+
+    // Percentage tresh arbitrarily chosen
+    strength = strength.toLocaleString(undefined, {style: (Math.abs(strength) < 3) ? "percent" : "decimal", signDisplay:"exceptZero", useGrouping: false});
+    
     return `${effect} ${strength}`;
 }
 function translateTarget(type, value) {

commit c810ca231fcc1b98b206323ca223d8d50e8f4878

Fix skill data percentages being rounded
Regression from f5b89d40

scripts/extract-skill-data.js
@@ -68,7 +68,7 @@ function translateEffect(type, strength) {
     }
 
     // Percentage tresh arbitrarily chosen
-    strength = strength.toLocaleString(undefined, {style: (Math.abs(strength) < 3) ? "percent" : "decimal", signDisplay:"exceptZero", useGrouping: false});
+    strength = strength.toLocaleString(undefined, {style: (Math.abs(strength) < 3) ? "percent" : "decimal", signDisplay:"exceptZero", useGrouping: false, maximumFractionDigits: 1});
     
     return `${effect} ${strength}`;
 }